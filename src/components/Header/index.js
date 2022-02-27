7b4d7871f6f8157a33f2008dc5fc5f2caff02113
Matias <matias.turra@gmail.com>
2022-02-27 19:03:23 -0300

    changes in map, api key added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+//Lib
 import { Autocomplete } from '@react-google-maps/api';
+// Styles
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
-
 import useStyles from './styles';
 
 const Header = () => {
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
+// Lib
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
 
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+    const coordinates = { lat:0, lng:0};
+
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyAPxBFRuzDzRBjFsl1kHIEWk5quOYaT_9A' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+
+            >
+
+            </GoogleMapReact>
+        </div>
     );
 }
 
